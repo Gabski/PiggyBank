7e14e07f6ab49aa436ba98c71e04677c80df47ef
Gabriel <g.koziestanski@createit.com>
2020-12-29 15:38:19 +0100

    accounts

--- src/main/java/piggy/bank/entity/Account.java
+++ src/main/java/piggy/bank/entity/Account.java
@@ -1,8 +1,12 @@
 package piggy.bank.entity;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import javax.persistence.Entity;
 import javax.persistence.*;
 import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Random;
 
 @Entity
 @Table(name = "account")
@@ -21,14 +25,35 @@ public class Account implements Serializable {
     @JoinColumn(name = "currency_id")
     private Currency currency;
 
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Column(name = "create_at")
+    private LocalDate createAt;
+
+    @Column(name = "bank_number")
+    private String bankNumber;
 
     public static Account create(User user, Currency currency) {
         Account account = new Account();
         account.setUser(user);
         account.setCurrency(currency);
+        account.setBankNumber(Account.randomBankNumber());
         return account;
     }
 
+    public static String randomBankNumber() {
+        String start = "PL";
+        Random value = new Random();
+
+        int r1 = value.nextInt(10);
+        int r2 = value.nextInt(10);
+        start += Integer.toString(r1) + Integer.toString(r2);
+        int n = 0;
+        for (int i = 0; i < 12; i++) {
+            n = value.nextInt(10);
+            start += Integer.toString(n);
+        }
+        return start;
+    }
 
     public Long getId() {
         return id;
@@ -52,5 +77,21 @@ public class Account implements Serializable {
 
     public void setCurrency(Currency currency) {
         this.currency = currency;
+    }
+
+    public LocalDate getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(LocalDate createAt) {
+        this.createAt = createAt;
+    }
+
+    public String getBankNumber() {
+        return bankNumber;
+    }
+
+    public void setBankNumber(String bankNumber) {
+        this.bankNumber = bankNumber;
     }
 }
---
+++ src/main/java/piggy/bank/service/UserService.java
@@ -0,0 +1,28 @@
+package piggy.bank.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import piggy.bank.entity.User;
+import piggy.bank.repository.UserRepository;
+
+
+@Service("userService")
+public class UserService implements UserServiceInterface {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public User findByUsernameOrEmail(String usernameOrEmail) {
+        User user = null;
+        try {
+            user = userRepository.findByUsernameOrEmail(usernameOrEmail);
+        } catch (Exception e) {
+            throw e;
+        }
+        return user;
+    }
+
+}
